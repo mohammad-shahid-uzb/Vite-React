490f885ca4b8921086063e19a3da2755e160e428
Mohammad Shahid <shahid.uzb@gmail.com>
2023-07-08 12:51:20 +0500

    images

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,6 @@
 import "./ListGroup.css";
 import Logo from "../assets/akros_trans.png";
-//import background from "../assets/Samarkand.jpeg";
+import nsp from "../assets/nsplsh.jpg";
 import uzb from "../assets/uzb.jpg";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
@@ -71,10 +71,10 @@ function ListGroup() {
       </div>
       <div
         style={{
-          height: "800px",
+          height: "600px",
           width: "100%",
           backgroundImage: `url(${uzb})`,
-          position: "relative",
+          position: "absolute",
           textAlign: "center",
           color: "Black",
         }}
@@ -82,15 +82,116 @@ function ListGroup() {
         <div
           className="centered"
           style={{
-            position: "absolute",
+            position: "relative",
             top: "40px",
-            left: "300px",
           }}
         >
           <p style={{ color: "Blue", fontSize: "60px" }}>
             College Admissions Guidance
           </p>
         </div>
+        <div
+          style={{
+            background: "#b2ffff",
+            maxWidth: "80%",
+            position: "absolute",
+            marginLeft: "300px",
+            marginTop: "200px",
+          }}
+        >
+          <Stack direction="row" spacing={3}>
+            <div>
+              <p
+                style={{
+                  textAlign: "center",
+                  color: "Blue",
+                  fontSize: "17px",
+                  fontStyle: "bold",
+                }}
+              >
+                Application Full Package
+              </p>
+              <p
+                style={{
+                  textAlign: "center",
+                  color: "Green",
+                  fontSize: "15px",
+                  fontStyle: "bold",
+                  padding: "15px",
+                }}
+              >
+                The whole process, taken <br />
+                care of: from developing ideas <br />
+                in May to final commitment in April.
+                <br />
+                Book now for the 2023 cycle.
+              </p>
+              <Button color="secondary">Our Offers</Button>
+            </div>
+            <div>
+              <p
+                style={{
+                  textAlign: "center",
+                  color: "Blue",
+                  fontSize: "17px",
+                  fontStyle: "bold",
+                }}
+              >
+                Application Essay Help
+              </p>
+              <p
+                style={{
+                  textAlign: "center",
+                  color: "Green",
+                  fontSize: "15px",
+                  fontStyle: "bold",
+                  padding: "15px",
+                }}
+              >
+                The individual attention any applicant <br />
+                needs to brainstorm, craft,
+                <br /> and perfect any to all of the <br />
+                required application writings.
+              </p>
+              <Button color="secondary">Our Offers</Button>
+            </div>
+            <div>
+              <p
+                style={{
+                  textAlign: "center",
+                  color: "Blue",
+                  fontSize: "17px",
+                  fontStyle: "bold",
+                }}
+              >
+                On-Demand Counseling
+              </p>
+              <p
+                style={{
+                  textAlign: "center",
+                  color: "Green",
+                  fontSize: "15px",
+                  fontStyle: "bold",
+                  padding: "15px",
+                }}
+              >
+                Insights on application strategy,
+                <br /> campus cultures, college choice, <br /> interview prep,
+                and the rest,
+                <br /> from recent graduates.
+              </p>
+              <Button color="secondary">Our Offers</Button>
+            </div>
+          </Stack>
+        </div>
+        <div
+          style={{
+            height: "1400px",
+            width: "95%",
+            backgroundImage: `url(${nsp})`,
+            marginTop: "470px",
+          }}
+        ></div>
       </div>
     </>
   );
