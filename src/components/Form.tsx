999bde962ff1c72f16a567a8592b7b4b120b64fe
Mohammad Shahid <shahid.uzb@gmail.com>
2023-07-11 01:30:15 +0500

    admission page-2

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,46 +3,40 @@ import emailjs from "@emailjs/browser";
 import "./Form.css";
 
 const ContactForm = () => {
-  const form = useRef();
-  const sendEmail = async (e) => {
-    e.preventDefault();
+  const form = useRef(null);
+
+  const sendEmail = async (event) => {
+    event.preventDefault();
     await emailjs
       .sendForm(
         "service_od0rubi",
         "template_6cvpatk",
-        form.current,
+        form.current!,
         "72ofXnNEBBUBs7NOF"
       )
       .then(
         (result) => {
-          e.target.reset();
+          event.target.reset();
           alert("message sent successfully...");
           console.log(result.text);
         },
         (error) => {
           alert("message not sent successfully...");
           console.log(error.text);
-          e.target.reset();
+          event.target.reset();
         }
       );
   };
 
   return (
     <form className="cf" ref={form} onSubmit={sendEmail}>
-      <h1 className="h1">Contact Us</h1>
-      <h2 className="h2">Schedule a college admissions consultation </h2>
       <label>Name</label>
       <input type="text" name="user_name" required />
       <label>Email</label>
       <input type="email" name="user_email" required />
       <label>Message</label>
       <textarea name="message" required />
-      <button
-        className="btn btn-primary"
-        type="submit"
-        value="Send"
-        style={{ margin: "10px" }}
-      >
+      <button className="btn btn-primary" type="submit" value="Send">
         Submit
       </button>
     </form>
