b3dab35f598d6baafbe7607d0897de938fc0ba26
Mohammad Shahid <shahid.uzb@gmail.com>
2023-07-20 22:29:52 +0500

    new json file-3

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,20 @@ import ResourcePage from "./components/ResourcePage";
 import SeminarPage from "./components/Seminars";
 import OurTeamPage from "./components/Team";
 import HomePage from "./components/Home";
-//import HeaderPage from "./components/Header";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
-      {/* <HeaderPage /> */}
       <Routes>
         <Route path="/seminars" element={<SeminarPage />} />
         <Route path="/team" element={<OurTeamPage />} />
         <Route path="/admissions" element={<AdmissionForm />} />
         <Route path="/resources" element={<ResourcePage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </div>
   );
